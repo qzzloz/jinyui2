45b9d1213efcdd816bbf81037afe14c31576705c
qzzloz <xlivsxd@gmail.com>
2024-07-30 13:37:52 +0900

    background, border 완료

---
+++ app/Http/Controllers/SampleController.php
@@ -0,0 +1,16 @@
+<?php
+
+// app/Http/Controllers/SampleController.php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SampleController extends Controller
+{
+    public function index()
+    {
+        return view('sample');
+    }
+}
+
---
+++ resources/views/sample.blade.php
@@ -0,0 +1,15 @@
+<x-www-app>
+  <div class="group p-9 sm:p-4">
+    <div class="group-hover:bg-red-400">안녕하세요</div>
+  </div>
+  <div class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-9 sm:p-8">안녕하세요</div>
+  <div class="p-9 sm:p-2" style="background-color: yellow;">안녕하세요</div>
+  <div class="columns-2xs bg-gradient-to-r from-purple-500 to-pink-500 sm:columns-2xl">zzzzz</div>
+  <div class="bg-gradient-to-r from-sky-500 to-indigo-500">HOHO</div>
+  <div class="flex items-center justify-center h-64 bg-gradient-to-r from-purple-500 to-pink-500 sm:columns-2xl">ㄹㅁㅈㄷㄹㅈㄱㅁㄷ</div>
+  <div class="p-4 shadow-sm bg-purple-500 w-16 h-16 rounded-full">222</div>
+  <div class="p-4 shadow-sm bg-blue-500 w-16 h-16 hover:rounded-t-lg">333</div>
+  <div class="p-4 bg-white w-16 h-16 border-8 border-indigo-600">444</div>
+  <div class="p-4 bg-white w-16 h-16 border-indigo-500 border-t-4">dsf</div>
+  <button class="px-4 py-2 font-semibold text-sm bg-white text-slate-700 dark:bg-slate-700 dark:text-white rounded-md shadow-sm ring-1 ring-slate-900/5 border-indigo-500 dark:border-sky-500 border-4 hover:border-double">Button A</button>
+</x-www-app>
